a16c5c3051c3b742bdb0864aa65769f235040f54
Matt Bennett <matt@bennett.name>
2014-08-07 13:10:41 +0100

    ...

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var swig  = require('swig');
 var app = express();
 
+app.use("/static", express.static(__dirname + '/static'));
 
 app.get('/', function(req, res){
     var template = swig.compileFile('templates/index.html');
